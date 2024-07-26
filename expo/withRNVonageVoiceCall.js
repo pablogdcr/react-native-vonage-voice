636368d229e8e1a4135c1e06ecee3599a783423b
Pablo Giraud-Carrier <pablo@themobilefirst.co>
2024-07-26 15:53:32 +0200

    feat: fix

--- expo/withRNVonageVoiceCall.js
+++ expo/withRNVonageVoiceCall.js
@@ -2,21 +2,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.withXcodeLinkBinaryWithLibraries = void 0;
 const config_plugins_1 = require("expo/config-plugins");
 
-const withXcodeLinkBinaryWithLibraries = (config, { library, status }) => {
-  return (0, config_plugins_1.withXcodeProject)(config, (config) => {
-      const options = status === "optional" ? { weak: true } : {};
-      const target = config_plugins_1.IOSConfig.XcodeUtils.getApplicationNativeTarget({
-          project: config.modResults,
-          projectName: config.modRequest.projectName,
-      });
-      config.modResults.addFramework(library, {
-          target: target.uuid,
-          ...options,
-      });
-      return config;
-  });
-};
-
 const withIosVonageVoiceCall = (config) => {
   config = (0, config_plugins_1.withInfoPlist)(config, (config) => {
     if (!Array.isArray(config.modResults.UIBackgroundModes)) {
@@ -37,7 +22,23 @@ const withIosVonageVoiceCall = (config) => {
   return config;
 }
 
-module.exports = function withVoipPushNotification(config) {
+const withXcodeLinkBinaryWithLibraries = (config, { library, status }) => {
+  return (0, config_plugins_1.withXcodeProject)(config, (config) => {
+      const options = status === "optional" ? { weak: true } : {};
+      const target = config_plugins_1.IOSConfig.XcodeUtils.getApplicationNativeTarget({
+          project: config.modResults,
+          projectName: config.modRequest.projectName,
+      });
+      config.modResults.addFramework(library, {
+          target: target.uuid,
+          ...options,
+      });
+      return config;
+  });
+};
+
+exports.withXcodeLinkBinaryWithLibraries = withXcodeLinkBinaryWithLibraries;
+exports.default = function withVoipPushNotification(config) {
   config = withIosVonageVoiceCall(config);
   // config = withAndroidVoipPushNotification(config);
 
