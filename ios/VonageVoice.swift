caf4717ae4b14c8c9bc26c29c806dfe7286aff38
Pablo Giraud-Carrier <pablo@themobilefirst.co>
2024-10-15 10:24:53 +0200

    fix: conflicts

--- ios/VonageVoice.swift
+++ ios/VonageVoice.swift
@@ -739,11 +739,11 @@ struct Constants {
         let audioSession = AVAudioSession.sharedInstance()
 
         do {
-            try audioSession.setCategory(.playAndRecord, mode: .voiceChat, options: [])
-            try audioSession.setActive(true)
-            VGVoiceClient.enableAudio(audioSession)
+          try audioSession.setCategory(.playAndRecord, mode: .voiceChat, options: [])
+          try audioSession.setActive(true)
+          VGVoiceClient.enableAudio(audioSession)
         } catch {
-            CustomLogger.logSlack(message: ":loud_sound: Failed to disable speaker\nid: \(String(describing: callID))\nerror: \(String(describing: error))")
+          CustomLogger.logSlack(message: ":loud_sound: Failed to disable speaker\nid: \(String(describing: callID))\nerror: \(String(describing: error))")
         }
         EventEmitter.shared.sendEvent(withName: Event.callAnswered.rawValue, body: ["callId": callId])
         break
