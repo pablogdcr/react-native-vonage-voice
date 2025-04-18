4a5c115d44263c9dac2f25e2934e9d7c3b6c7667
Louis <duboscq.louis@gmail.com>
2025-04-18 15:30:15 +0200

    feat: adds unregisterDeviceTokens

--- android/src/main/java/com/vonagevoice/js/VonageVoiceModule.kt
+++ android/src/main/java/com/vonagevoice/js/VonageVoiceModule.kt
@@ -15,12 +15,13 @@ import com.facebook.react.bridge.ReactMethod
 import com.facebook.react.bridge.ReadableMap
 import com.vonagevoice.audio.DeviceManager
 import com.vonagevoice.audio.SpeakerController
-import com.vonagevoice.audio.getAvailableAudioOutputs
 import com.vonagevoice.auth.IVonageAuthenticationService
 import com.vonagevoice.call.ICallActionsHandler
 import com.vonagevoice.call.VonagePushMessageService
+import com.vonagevoice.storage.VonageStorage
 import com.vonagevoice.utils.success
 import com.vonagevoice.utils.tryBlocking
+import com.vonagevoice.utils.tryNotBlocking
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -28,101 +29,114 @@ import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 
 class VonageVoiceModule(reactContext: ReactApplicationContext) :
-    ReactContextBaseJavaModule(reactContext), KoinComponent {
+  ReactContextBaseJavaModule(reactContext), KoinComponent {
 
-    private val vonageAuthenticationService: IVonageAuthenticationService by inject()
-    private val speakerController: SpeakerController by inject()
-    private val callActionsHandler: ICallActionsHandler by inject()
-    private val jsEventSender: JSEventSender by inject()
-    private val deviceManager: DeviceManager by inject()
-    private val scope = CoroutineScope(Dispatchers.IO)
-    private var toneGenerator: ToneGenerator? = null
+  private val vonageAuthenticationService: IVonageAuthenticationService by inject()
+  private val speakerController: SpeakerController by inject()
+  private val callActionsHandler: ICallActionsHandler by inject()
+  private val jsEventSender: JSEventSender by inject()
+  private val vonageStorage: VonageStorage by inject()
+  private val deviceManager: DeviceManager by inject()
+  private val scope = CoroutineScope(Dispatchers.IO)
+  private var toneGenerator: ToneGenerator? = null
 
-    override fun getName(): String {
-        return "VonageVoiceModule"
+  override fun getName(): String {
+    return "VonageVoiceModule"
+  }
+
+  override fun getConstants(): MutableMap<String, Any> {
+    return hashMapOf()
+  }
+
+  @ReactMethod
+  fun saveDebugAdditionalInfo(info: String) {
+    Log.d("VonageVoiceModule", "saveDebugAdditionalInfo $info")
+  }
+
+  @ReactMethod
+  fun setRegion(region: String) {
+    Log.d("VonageVoiceModule", "setRegion $region")
+    vonageAuthenticationService.setRegion(region)
+  }
+
+  @ReactMethod
+  fun login(jwt: String, promise: Promise) {
+    Log.d("VonageVoiceModule", "login $jwt")
+    scope.launch { promise.tryBlocking { vonageAuthenticationService.login(jwt) } }
+  }
+
+  @ReactMethod
+  fun logout(promise: Promise) {
+    Log.d("VonageVoiceModule", "logout")
+    scope.launch { promise.tryBlocking { vonageAuthenticationService.logout() } }
+  }
+
+  /**
+   * isSandbox looks used only for iOS, we ignore it here
+   * This method is triggered by react native when event register is sent by event emitter
+   * @param token = push token from firebase
+   */
+  @ReactMethod
+  fun registerVonageVoipToken(token: String, isSandbox: Boolean, promise: Promise) {
+    Log.d("VonageVoiceModule", "registerVonageVoipToken $token")
+    scope.launch {
+      promise.tryBlocking {
+        vonageAuthenticationService.registerVonageVoipToken(newTokenFirebase = token)
+      }
     }
+  }
 
-    override fun getConstants(): MutableMap<String, Any> {
-        return hashMapOf()
+  @ReactMethod
+  fun unregisterDeviceTokens(deviceId: String, promise: Promise) {
+    Log.d("VonageVoiceModule", "unregisterDeviceTokens $deviceId")
+    with(vonageStorage) {
+      removePushToken()
+      removeDeviceId()
     }
+  }
 
-    @ReactMethod
-    fun saveDebugAdditionalInfo(info: String) {
-        Log.d("VonageVoiceModule", "saveDebugAdditionalInfo $info")
-    }
-
-    @ReactMethod
-    fun setRegion(region: String) {
-        Log.d("VonageVoiceModule", "setRegion $region")
-        vonageAuthenticationService.setRegion(region)
-    }
-
-    @ReactMethod
-    fun login(jwt: String, promise: Promise) {
-        Log.d("VonageVoiceModule", "login $jwt")
-        scope.launch { promise.tryBlocking { vonageAuthenticationService.login(jwt) } }
-    }
-
-    @ReactMethod
-    fun logout(promise: Promise) {
-        Log.d("VonageVoiceModule", "logout")
-        scope.launch { promise.tryBlocking { vonageAuthenticationService.logout() } }
-    }
-
-    /**
-     * isSandbox looks used only for iOS, we ignore it here
-     * This method is triggered by react native when event register is sent by event emitter
-     * @param token = push token from firebase
-     */
-    @ReactMethod
-    fun registerVonageVoipToken(token: String, isSandbox: Boolean, promise: Promise) {
-        Log.d("VonageVoiceModule", "registerVonageVoipToken $token")
-        scope.launch {
-            promise.tryBlocking {
-                vonageAuthenticationService.registerVonageVoipToken(newTokenFirebase = token)
-            }
+  @ReactMethod
+  fun requestFullIntentPermission(promise: Promise) {
+    Log.d("VonageVoiceModule", "requestFullIntentPermission")
+    scope.launch {
+      promise.tryBlocking {
+        if (Build.VERSION.SDK_INT > 33) {
+          val intent = Intent(Settings.ACTION_MANAGE_APP_USE_FULL_SCREEN_INTENT).apply {
+            data = Uri.fromParts("package", reactApplicationContext.packageName, null)
+          }
+          reactApplicationContext.startActivity(intent)
         }
+      }
     }
 
     @ReactMethod
     fun unregisterDeviceTokens(deviceId: String, promise: Promise) {
-        Log.d("VonageVoiceModule", "unregisterDeviceTokens $deviceId")
-    }
-
-    @ReactMethod
-    fun requestFullIntentPermission(promise: Promise) {
-        Log.d("VonageVoiceModule", "requestFullIntentPermission")
-        scope.launch {
-            promise.tryBlocking {
-                if (Build.VERSION.SDK_INT > 33) {
-                    val intent = Intent(Settings.ACTION_MANAGE_APP_USE_FULL_SCREEN_INTENT).apply {
-                        data = Uri.fromParts("package", reactApplicationContext.packageName, null)
-                    }
-                    reactApplicationContext.startActivity(intent)
-                }
-            }
-        }
+      Log.d("VonageVoiceModule", "unregisterDeviceTokens $deviceId")
+      with(vonageStorage) {
+        removePushToken()
+        removeDeviceId()
+      }
     }
 
     @ReactMethod
     fun answerCall(callId: String, promise: Promise) {
-        val normalizedCallId = callId.lowercase()
-        Log.d("VonageVoiceModule", "answerCall $normalizedCallId")
-        scope.launch { callActionsHandler.answer(normalizedCallId) }
+      val normalizedCallId = callId.lowercase()
+      Log.d("VonageVoiceModule", "answerCall $normalizedCallId")
+      scope.launch { callActionsHandler.answer(normalizedCallId) }
     }
 
     @ReactMethod
     fun rejectCall(callId: String, promise: Promise) {
-        val normalizedCallId = callId.lowercase()
-        Log.d("VonageVoiceModule", "rejectCall $normalizedCallId")
-        scope.launch { promise.tryBlocking { callActionsHandler.reject(normalizedCallId) } }
+      val normalizedCallId = callId.lowercase()
+      Log.d("VonageVoiceModule", "rejectCall $normalizedCallId")
+      scope.launch { promise.tryBlocking { callActionsHandler.reject(normalizedCallId) } }
     }
 
     @ReactMethod
     fun hangup(callId: String, promise: Promise) {
-        val normalizedCallId = callId.lowercase()
-        Log.d("VonageVoiceModule", "hangup $normalizedCallId")
-        scope.launch { promise.tryBlocking { callActionsHandler.hangup(normalizedCallId) } }
+      val normalizedCallId = callId.lowercase()
+      Log.d("VonageVoiceModule", "hangup $normalizedCallId")
+      scope.launch { promise.tryBlocking { callActionsHandler.hangup(normalizedCallId) } }
     }
 
     @ReactMethod
@@ -141,109 +155,112 @@ class VonageVoiceModule(reactContext: ReactApplicationContext) :
 
             promise.tryBlocking { }
         }
+      }
     }
 
     @ReactMethod
     fun sendDTMF(dtmf: String, promise: Promise) {
-        Log.d("VonageVoiceModule", "sendDTMF $dtmf")
-        scope.launch {
-            promise.tryBlocking {
-                callActionsHandler.sendDTMF(dtmf)
-                Log.d("VonageVoiceModule", "sendDTMF $dtmf done")
-            }
+      Log.d("VonageVoiceModule", "sendDTMF $dtmf")
+      scope.launch {
+        promise.tryBlocking {
+          callActionsHandler.sendDTMF(dtmf)
+          Log.d("VonageVoiceModule", "sendDTMF $dtmf done")
         }
+      }
     }
 
     @ReactMethod
     fun reconnectCall(callId: String, promise: Promise) {
-        val normalizedCallId = callId.lowercase()
-        Log.d("VonageVoiceModule", "reconnectCall $normalizedCallId")
-        scope.launch { promise.tryBlocking { callActionsHandler.reconnectCall(normalizedCallId) } }
+      val normalizedCallId = callId.lowercase()
+      Log.d("VonageVoiceModule", "reconnectCall $normalizedCallId")
+      scope.launch { promise.tryBlocking { callActionsHandler.reconnectCall(normalizedCallId) } }
     }
 
     @ReactMethod
     fun getAvailableAudioDevices(promise: Promise) {
-        Log.d("VonageVoiceModule", "getAvailableAudioDevices")
+      Log.d("VonageVoiceModule", "getAvailableAudioDevices")
 
-        try {
-            val inputs = deviceManager.getAvailableAudioOutputs(reactApplicationContext)
-            val array = Arguments.createArray()
+      try {
+        val inputs = deviceManager.getAvailableAudioOutputs(reactApplicationContext)
+        val array = Arguments.createArray()
 
-            inputs.forEach { input ->
-                val device = Arguments.createMap().apply {
-                    putString("name", input.name)
-                    putString("id", input.id)
-                    putString("type", input.type)
-                }
-                array.pushMap(device)
-            }
-
-            promise.resolve(array)
-        } catch (e: Exception) {
-            promise.reject("AUDIO_INPUT_ERROR", e.message, e)
+        inputs.forEach { input ->
+          val device = Arguments.createMap().apply {
+            putString("name", input.name)
+            putString("id", input.id)
+            putString("type", input.type)
+          }
+          array.pushMap(device)
         }
+
+        promise.resolve(array)
+      } catch (e: Exception) {
+        promise.reject("AUDIO_INPUT_ERROR", e.message, e)
+      }
     }
 
     @ReactMethod
     fun setAudioDevice(deviceId: String, promise: Promise) {
-        Log.d("VonageVoiceModule", "setAudioDevice $deviceId")
+      Log.d("VonageVoiceModule", "setAudioDevice $deviceId")
 
-        promise.tryNotBlocking {
-            deviceManager.setAudioDevice(deviceId.toIntOrNull() ?: throw IllegalStateException("Device id not supported $deviceId"))
-        }
+      promise.tryNotBlocking {
+        deviceManager.setAudioDevice(
+          deviceId.toIntOrNull() ?: throw IllegalStateException("Device id not supported $deviceId")
+        )
+      }
     }
 
 
     @ReactMethod
     fun addListener(eventName: String) {
-        Log.d("VonageVoiceModule", "addListener $eventName")
+      Log.d("VonageVoiceModule", "addListener $eventName")
     }
 
     @ReactMethod
     fun removeListeners(count: Int) {
-        Log.d("VonageVoiceModule", "removeListeners $count")
+      Log.d("VonageVoiceModule", "removeListeners $count")
     }
 
     @ReactMethod
     fun mute(callId: String, promise: Promise) {
-        val normalizedCallId = callId.lowercase()
-        Log.d("VonageVoiceModule", "mute $normalizedCallId")
+      val normalizedCallId = callId.lowercase()
+      Log.d("VonageVoiceModule", "mute $normalizedCallId")
 
-        scope.launch { promise.tryBlocking { callActionsHandler.mute(normalizedCallId) } }
+      scope.launch { promise.tryBlocking { callActionsHandler.mute(normalizedCallId) } }
     }
 
     @ReactMethod
     fun unmute(callId: String, promise: Promise) {
-        val normalizedCallId = callId.lowercase()
-        Log.d("VonageVoiceModule", "unmute $normalizedCallId")
+      val normalizedCallId = callId.lowercase()
+      Log.d("VonageVoiceModule", "unmute $normalizedCallId")
 
-        scope.launch { promise.tryBlocking { callActionsHandler.unmute(normalizedCallId) } }
+      scope.launch { promise.tryBlocking { callActionsHandler.unmute(normalizedCallId) } }
     }
 
     @ReactMethod
     fun enableSpeaker(promise: Promise) {
-        Log.d("VonageVoiceModule", "enableSpeaker")
-        speakerController.enableSpeaker()
+      Log.d("VonageVoiceModule", "enableSpeaker")
+      speakerController.enableSpeaker()
     }
 
     @ReactMethod
     fun disableSpeaker(promise: Promise) {
-        Log.d("VonageVoiceModule", "disableSpeaker")
-        speakerController.disableSpeaker()
+      Log.d("VonageVoiceModule", "disableSpeaker")
+      speakerController.disableSpeaker()
     }
 
     @ReactMethod
     fun registerVoipToken(promise: Promise) {
-        Log.d("VonageVoiceModule", "registerVoipToken")
+      Log.d("VonageVoiceModule", "registerVoipToken")
 
-        scope.launch {
-            jsEventSender.sendFirebasePushToken(VonagePushMessageService.requestToken())
-        }
+      scope.launch {
+        jsEventSender.sendFirebasePushToken(VonagePushMessageService.requestToken())
+      }
     }
 
     override fun invalidate() {
-        super.invalidate()
-        Log.d("VonageVoiceModule", "invalidate")
+      super.invalidate()
+      Log.d("VonageVoiceModule", "invalidate")
     }
 
 
@@ -252,45 +269,45 @@ class VonageVoiceModule(reactContext: ReactApplicationContext) :
 
     @ReactMethod
     fun subscribeToAudioRouteChange() {
-        Log.d("VonageVoiceModule", "subscribeToAudioRouteChange")
+      Log.d("VonageVoiceModule", "subscribeToAudioRouteChange")
 
     }
 
 
     @ReactMethod
     fun subscribeToCallEvents() {
-        Log.d("VonageVoiceModule", "subscribeToCallEvents")
+      Log.d("VonageVoiceModule", "subscribeToCallEvents")
 
-        // Done in CallActionsHandler::init
+      // Done in CallActionsHandler::init
     }
 
     @ReactMethod
     fun unsubscribeFromCallEvents() {
-        Log.d("VonageVoiceModule", "unsubscribeFromCallEvents")
+      Log.d("VonageVoiceModule", "unsubscribeFromCallEvents")
     }
 
     @ReactMethod
     fun playDTMFTone(key: String, promise: Promise) {
-        Log.d("VonageVoiceModule", "playDTMFTone called with key: $key")
+      Log.d("VonageVoiceModule", "playDTMFTone called with key: $key")
 
-        val tone = when (key) {
-            "0" -> ToneGenerator.TONE_DTMF_0
-            "1" -> ToneGenerator.TONE_DTMF_1
-            "2" -> ToneGenerator.TONE_DTMF_2
-            "3" -> ToneGenerator.TONE_DTMF_3
-            "4" -> ToneGenerator.TONE_DTMF_4
-            "5" -> ToneGenerator.TONE_DTMF_5
-            "6" -> ToneGenerator.TONE_DTMF_6
-            "7" -> ToneGenerator.TONE_DTMF_7
-            "8" -> ToneGenerator.TONE_DTMF_8
-            "9" -> ToneGenerator.TONE_DTMF_9
-            "*" -> ToneGenerator.TONE_DTMF_S
-            "#" -> ToneGenerator.TONE_DTMF_P
-            else -> {
-                promise.reject("INVALID_KEY", "Invalid DTMF key: $key")
-                return
-            }
+      val tone = when (key) {
+        "0" -> ToneGenerator.TONE_DTMF_0
+        "1" -> ToneGenerator.TONE_DTMF_1
+        "2" -> ToneGenerator.TONE_DTMF_2
+        "3" -> ToneGenerator.TONE_DTMF_3
+        "4" -> ToneGenerator.TONE_DTMF_4
+        "5" -> ToneGenerator.TONE_DTMF_5
+        "6" -> ToneGenerator.TONE_DTMF_6
+        "7" -> ToneGenerator.TONE_DTMF_7
+        "8" -> ToneGenerator.TONE_DTMF_8
+        "9" -> ToneGenerator.TONE_DTMF_9
+        "*" -> ToneGenerator.TONE_DTMF_S
+        "#" -> ToneGenerator.TONE_DTMF_P
+        else -> {
+          promise.reject("INVALID_KEY", "Invalid DTMF key: $key")
+          return
         }
+      }
 
         try {
             // Stop any existing tone
@@ -316,7 +333,7 @@ class VonageVoiceModule(reactContext: ReactApplicationContext) :
 
     @ReactMethod
     fun stopDTMFTone(promise: Promise) {
-        Log.d("VonageVoiceModule", "stopDTMFTone called")
+      Log.d("VonageVoiceModule", "stopDTMFTone called")
 
         try {
             toneGenerator?.stopTone()
@@ -331,4 +348,4 @@ class VonageVoiceModule(reactContext: ReactApplicationContext) :
             promise.reject("TONE_STOP_ERROR", "Failed to stop tone", e)
         }
     }
-}
+  }
